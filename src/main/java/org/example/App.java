fc941a7c21b6f2aec31d5b36a418d1a0cc466fab
Mihan <misha-kokorin@yandex.ru>
2020-01-02 13:34:24 +0300

    rewrite WeatherBotVk without parsing JSON

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,37 +1,30 @@
 package org.example;
 
-import com.vk.api.sdk.client.TransportClient;
-import com.vk.api.sdk.client.VkApiClient;
-import com.vk.api.sdk.client.actors.GroupActor;
-import com.vk.api.sdk.exceptions.ApiException;
-import com.vk.api.sdk.exceptions.ClientException;
-import com.vk.api.sdk.httpclient.HttpTransportClient;
-import com.vk.api.sdk.queries.groups.GroupsGetLongPollServerQuery;
+import com.petersamokhin.bots.sdk.clients.Group;
+import com.petersamokhin.bots.sdk.objects.Message;
 
 import java.io.IOException;
 
 public class App {
 
-	//started to change of using api
-	//another useless test comment
+    Group group = new Group(188205376, "29b9c2b8bc68974b87ebb9447649a016125edd478ff9d7de530af31e89f6232d8ba19cd8d6a64390f8ddd");
+    String text = "";
+    String messageToSend = "";
 
-    TransportClient transportClient = HttpTransportClient.getInstance();
-    VkApiClient vk = new VkApiClient(transportClient);
-    GroupActor actor = new GroupActor(188205376, "29b9c2b8bc68974b87ebb9447649a016125edd478ff9d7de530af31e89f6232d8ba19cd8d6a64390f8ddd");
-    GroupsGetLongPollServerQuery a = vk.groups().getLongPollServer(actor);
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) throws ClientException, ApiException, IOException {
+        App app = new App();
+
         while (true) {
-            App app = new App();
-            MessageBotVK messageBotVK = new MessageBotVK();
-            messageBotVK.getMessage(new App());
-            app.vk.messages().send(app.actor, 312138559).message("You have been said: " + messageBotVK.text).execute();
-            try {
-                String messageToSend = Weather.getWeather(new ModelJSON(), messageBotVK);
-                app.vk.messages().send(app.actor, 312138559).message(messageToSend).execute();
-            } catch (Exception e) {
-                app.vk.messages().send(app.actor, 312138559).message("City is not found!").execute();
-            }
+            app.group.onSimpleTextMessage(message -> {
+                try {
+                    app.text = message.getText();
+                    app.messageToSend = Weather.getWeather(new ModelJSON(), app);
+                    new Message().from(app.group).to(message.authorId()).text(app.messageToSend).send();
+                } catch (IOException e) {
+                    new Message().from(app.group).to(message.authorId()).text("Такого города не существует!").send();
+                }
+            });
         }
     }
 }
